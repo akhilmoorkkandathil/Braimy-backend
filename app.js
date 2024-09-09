eb1dec4378f827cb2c182f62cc6489c14043a094
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-09-09 23:30:50 +0530

    Node Deployment to EC2

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static('public'));
 
-const allowedOrigins = [process.env.BASE_URL_CLIENT,'http://127.0.0.1:8081','http://10.20.4.220:8081'];
+const allowedOrigins = ["http://16.171.148.207:4200",'http://127.0.0.1:8081','http://10.20.4.220:8081'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
